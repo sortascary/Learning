075348a572404a07f869b09cd24e797c47f28fb7
sortascary <dkaky2008@gmail.com>
2024-09-03 08:39:37 +0700

    feat: [Dzak]between

---
+++ lib/Widgets/Between.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+class Between extends HookWidget {
+  const Between({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            // Container(
+            //   margin: EdgeInsets.all(10),
+            //   child: ElevatedButton(
+            //       onPressed: () {
+            //         Navigator.push(
+            //           context,
+            //           MaterialPageRoute(builder: (context) => BMICalculator()),
+            //         );
+            //       },
+            //       child: Text('BMI')),
+            // ),
+            // Container(
+            //   margin: EdgeInsets.all(10),
+            //   child: ElevatedButton(
+            //       onPressed: () {
+            //         Navigator.push(
+            //           context,
+            //           MaterialPageRoute(builder: (context) => CardListView()),
+            //         );
+            //       },
+            //       child: Text('Bangun datar')),
+            // ),
+
+            // Container(
+            //   margin: EdgeInsets.all(10),
+            //   child: ElevatedButton(
+            //       onPressed: () {
+            //         Navigator.push(
+            //           context,
+            //           MaterialPageRoute(builder: (context) => Arithmetic()),
+            //         );
+            //       },
+            //       child: Text('Arirmatika')),
+            // ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:learnhookflutter/Widgets/Between.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'BMI',
+      home: const Between(),
+    );
+    
+  }
+}
